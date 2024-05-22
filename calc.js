2f5cadf512eeec8f93e5052fcbde0132dd975fce
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-05-22 16:16:12 -0400

    add eventlisteners to all calculator keys

--- calc.js
+++ calc.js
@@ -1,3 +1,25 @@
+const calculator = document.querySelector("#bigscreen");
+const keys = calculator.querySelectorAll("button");
+
+keys.forEach((buttons) => {
+  buttons.addEventListener("click", (e) => {
+    if (e.target.matches("button")) {
+      console.log("I've been pressed");
+    }
+    if (!e.target.dataset.action) {
+      console.log("number key!");
+    }
+    if (
+      e.target.dataset.action === "add" ||
+      e.target.dataset.action === "subtract" ||
+      e.target.dataset.action === "multiply" ||
+      e.target.dataset.action === "divide"
+    ) {
+      console.log("operator key!");
+    }
+  });
+});
+
 const add = function (num1, num2) {
   return num1 + num2;
 };
