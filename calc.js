9cc5bc7d56ed5dcf99a0f187aa2711e70066eb77
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-05-28 14:13:36 -0400

    all done

--- calc.js
+++ calc.js
@@ -1,44 +1,74 @@
 const calculator = document.querySelector("#bigscreen");
 const keys = calculator.querySelectorAll("button");
+const display = document.querySelector("#display");
 
 keys.forEach((buttons) => {
   buttons.addEventListener("click", (e) => {
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
     if (e.target.matches("button")) {
       console.log("I've been pressed");
     }
-    if (!e.target.dataset.action) {
-      console.log("number key!");
+    if (!action) {
+      if (dispatchEvent === "0") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
     }
     if (
-      e.target.dataset.action === "add" ||
-      e.target.dataset.action === "subtract" ||
-      e.target.dataset.action === "multiply" ||
-      e.target.dataset.action === "divide"
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
     ) {
-      console.log("operator key!");
+      key.classList.add("is--depressed");
+      calculator.dataset.previousKeyType = "operator";
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+    if (action === "decimal") {
+      display.textContent = displayedNum + ".";
+    }
+    Array.from(key.parentNode.children).forEach((k) =>
+      k.classList.remove(".key--operator:focus")
+    );
+
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    if (!action) {
+      if (displayedNum === "0" || previousKeyType === "operator") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+    const calculate = (n1, operator, n2) => {
+      let result = "";
+      if (operator === "add") {
+        result = parseFloat(n1) + parseFloat(n2);
+      } else if (operator === "subtract") {
+        result = parseFloat(n1) - parseFloat(n2);
+      } else if (operator === "multiply") {
+        result = parseFloat(n1) * parseFloat(n2);
+      } else if (operator === "divide") {
+        result = parseFloat(n1) / parseFloat(n2);
+      }
+
+      return result;
+    };
+    if (action === "calculate") {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+
+    if (action === "clear") {
+      display.textContent = "0";
     }
   });
 });
-
-const add = function (num1, num2) {
-  return num1 + num2;
-};
-
-const subtract = function (num1, num2) {
-  return num1 - num2;
-};
-
-const divide = function (num1, num2) {
-  return num1 / num2;
-};
-
-const multiply = function (num1, num2) {
-  return num1 * num2;
-};
-
-let firstNumber = 0;
-let secondNumber = 0;
-
-function calculate(firstNumber, secondNumber, operator) {
-  console.log(firstNumber, secondNumber, operator);
-}
